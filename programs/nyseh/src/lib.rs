9eed319e708d822bfe00bed1964e35f39dc158e1
shifkey <sevensidedsilver@gmail.com>
2026-03-06 11:12:42 -0800

    hook in but trait bound errors I think from imports

--- programs/nyseh/src/lib.rs
+++ programs/nyseh/src/lib.rs
@@ -1,17 +1,29 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{
+    prelude::*,
+    system_program::{create_account, CreateAccount},
+};
 use anchor_spl::{
     associated_token::AssociatedToken,
     metadata::{
         create_metadata_accounts_v3, mpl_token_metadata::types::DataV2, CreateMetadataAccountsV3,
         Metadata as Metaplex,
     },
-    token::{mint_to, Mint, MintTo, Token, TokenAccount},
+    token::Token,
+    token_interface::{mint_to, Mint, MintTo, TokenAccount, TokenInterface},
+};
+use spl_tlv_account_resolution::{
+    account::ExtraAccountMeta, seeds::Seed, state::ExtraAccountMetaList,
+};
+use spl_transfer_hook_interface::instruction::{
+    ExecuteInstruction, InitializeExtraAccountMetaListInstruction, TransferHookInstruction,
 };
 
 declare_id!("4QYEwXsTut3ourrNjbHN8DJYLNPVLLZ3WGoYgH9vG9aR");
 
 #[program]
 mod nyseh {
+    use spl_transfer_hook_interface::instruction::InitializeExtraAccountMetaListInstruction;
+
     use super::*;
 
     pub fn initiate_token(ctx: Context<InitToken>, metadata: InitTokenParams) -> Result<()> {
@@ -65,7 +77,94 @@ mod nyseh {
         )?;
         Ok(())
     }
+    pub fn initialize_extra_account_meta_list(
+        ctx: Context<InitializeExtraAccountMetaList>,
+    ) -> Result<()> {
+        let account_metas: Vec<ExtraAccountMeta> = vec![];
+        let account_size: u64 = ExtraAccountMetaList::size_of(account_metas.len()).unwrap() as u64;
+        let lamports: u64 = Rent::get()?.minimum_balance(account_size as usize);
+
+        let mint: Pubkey = ctx.accounts.mint.key();
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"extra-account-metas",
+            &mint.as_ref(),
+            &[ctx.bumps.extra_account_meta_list],
+        ]];
+
+        create_account(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                CreateAccount {
+                    from: ctx.accounts.payer.to_account_info(),
+                    to: ctx.accounts.extra_account_meta_list.to_account_info(),
+                },
+            )
+            .with_signer(signer_seeds),
+            lamports,
+            account_size,
+            ctx.program_id,
+        )?;
+        ExtraAccountMetaList::init::<ExecuteInstruction>(
+            &mut ctx.accounts.extra_account_meta_list.try_borrow_mut_data()?,
+            &account_metas,
+        );
+        Ok(())
+    }
+    pub fn transfer_hook(ctx: Context<TransferHook>, amount: u64) -> Result<()> {
+        msg!("hit the transfer hook!");
+        Ok(())
+    }
+    pub fn fallback<'info>(
+        program_id: &Pubkey,
+        accounts: &'info [AccountInfo<'info>],
+        data: &[u8],
+    ) -> Result<()> {
+        let instruction: TransferHookInstruction = TransferHookInstruction::unpack(data).unwrap();
+        match instruction {
+            TransferHookInstruction::Execute { amount } => {
+                let amount_bytes: [u8; _] = amount.to_le_bytes();
+
+                __private::__global::transfer_hook(program_id, accounts, &amount_bytes)?
+            }
+            _ => return Err(ProgramError::InvalidInstructionData.into()),
+        }
+        Ok(())
+    }
 }
+#[derive(Accounts)]
+pub struct InitializeExtraAccountMetaList<'info> {
+    #[account(mut)]
+    payer: Signer<'info>,
+
+    /// CHECK: ExtraAccountMetaList Account, must have seeds
+    #[account(
+        mut,
+        seeds = [b"extra-account-metas", mint.key().as_ref()],
+        bump
+    )]
+    pub extra_account_meta_list: AccountInfo<'info>,
+    pub mint: InterfaceAccount<'info, Mint>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+#[derive(Accounts)]
+pub struct TransferHook<'info> {
+    #[account(
+        token::mint=mint,
+        token::authority=owner,
+    )]
+    pub source_token: InterfaceAccount<'info, TokenAccount>,
+    pub mint: InterfaceAccount<'info, Mint>,
+    #[account(token::mint=mint)]
+    pub destination_token: InterfaceAccount<'info, TokenAccount>,
+    /// CHECK: source token owner, can be SystemAccount or PDA
+    pub owner: UncheckedAccount<'info>,
+    /// CHECK: ExtraAccountMetaList Account
+    #[account(seeds=[b"extra-account-metas", mint.key().as_ref()], bump)]
+    pub extra_account_meta_list: UncheckedAccount<'info>,
+}
+
 #[derive(Accounts)]
 pub struct MintTokens<'info> {
     #[account(
